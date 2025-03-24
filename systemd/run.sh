8d954bbe2046b0199080ffd80c2a69789c9766c1
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2025-03-25 08:37:43 +1100

    Update run

--- systemd/run.sh
+++ systemd/run.sh
@@ -3,6 +3,6 @@ export PYTHONPATH=$PYTHONPATH:/usr/local/src/nathan-source/drift-downloader
 touch /var/log/drift-downloader.log
 cd /usr/local/src/nathan-source/drift-downloader
 echo "Starting Drift Downloader $(date)" >> /var/log/drift-downloader.log
-uv sync >> /var/log/drift-downloader.log
-uv run app/main.py >> /var/log/drift-downloader.log
+/home/nathan/.local/bin/uv sync >> /var/log/drift-downloader.log
+/home/nathan/.local/bin/uv run app/main.py >> /var/log/drift-downloader.log
 echo "Drift Downloader stopped $(date)" >> /var/log/drift-downloader.log
