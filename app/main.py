ea9be92089c0f9e3c701ca54ddbed5a09b197ae4
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2025-03-30 15:15:46 +1100

    Fix up lockfile deleteand log typo

--- app/main.py
+++ app/main.py
@@ -4,10 +4,6 @@ from app.domain.use_cases.perform_connect_download import perform_connect_downlo
 from app.domain.use_cases.check_lock_file import CheckLockFile
 from app.domain.use_cases.create_lock_file import CreateLockFile
 from app.domain.use_cases.delete_lock_file import DeleteLockFile
-from app.domain.use_cases.download_files import download_files
-from app.domain.use_cases.connect_normal import connect_normal
-from app.domain.use_cases.connect_ghost import connect_ghost
-from app.domain.use_cases.get_file_list import get_file_list
 
 
 def main():
@@ -25,9 +21,9 @@ def main():
         perform_connect_download()
     except Exception as e:
         logger.exception(e)
-
-    logger.debug("Deleting Lock File")
-    DeleteLockFile().execute()
+    finally:
+        logger.debug("Deleting Lock File")
+        DeleteLockFile().execute()
 
     logger.info("Finished Check Download")
 
--- systemd/run.sh
+++ systemd/run.sh
@@ -2,8 +2,9 @@
 export PYTHONPATH=$PYTHONPATH:/usr/local/src/nathan-source/drift-downloader
 touch /var/log/drift-downloader.log
 cd /usr/local/src/nathan-source/drift-downloader
-echo "" >> /var/log/drift-download.log
+echo "" >> /var/log/drift-downloader.log
 echo "Starting Drift Downloader $(date)" >> /var/log/drift-downloader.log
 /home/nathan/.local/bin/uv sync >> /var/log/drift-downloader.log
 /home/nathan/.local/bin/uv run app/main.py >> /var/log/drift-downloader.log
 echo "Drift Downloader stopped $(date)" >> /var/log/drift-downloader.log
+
